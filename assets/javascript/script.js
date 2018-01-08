79735700e818155191a829b45cd81c7eba1497ce
Mike D <sprinkles@Mikes-MacBook-Pro.local>
2018-01-07 18:26:23 -0600

    moved button control/creation to top to see if that would fix issue. it did not.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,30 @@
 var gifs = ["winona ryder", "michael j fox", "tom hardy","lauren ambrose"];
+console.log("array:" + gifs);
+
+	function createButton () {
+		$("#display-buttons").empty();
+		for (var i = 0; i < gifs.length; i++) {
+		var b = $("<button>");
+		b.addClass("gif");
+		b.attr("data-name", gifs[i]);
+		b.text(gifs[i]);
+		$("#display-buttons").append(b);
+		}
+
+	}
+	$("#add-gif").on("click", function(event) {
+		event.preventDefault();
+
+		var newGif = $("#gif-input").val().trim();
+
+		gifs.push(newGif);
+
+		createButton();
+
+		// $("#gif-input").val('');
+
+	});		
+
 	function createGif () {
 		var gifGet = $(this).attr("data-name");
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifs + "&api_key=xuuNk345sik7Okl06nd5Ylgk7rTtDkop&limit=10";
@@ -24,32 +50,11 @@ var gifs = ["winona ryder", "michael j fox", "tom hardy","lauren ambrose"];
 	        });
 
 	}//closes createGif function
-		function createButton () {
-			$("#display-buttons").empty();
-			for (var i = 0; i < gifs.length; i++) {
-			var b = $("<button>");
-			b.addClass("gif");
-			b.attr("data-name", gifs[i]);
-			b.text(gifs[i]);
-			$("#display-buttons").append(b);
-			}
 
-		}
 
-	$("#add-gif").on("click", function(event) {
-		event.preventDefault();
-
-		var gif = $("#gif-input").val().trim();
-
-		gifs.push(gif);
-
-		createButton();
-
-		$("#gif-input").val('');
-
-	});	
-	createGif();	
+	
 
 	$(document).on("click", ".gifDiv", createGif);
 
 	createButton();	
+	createGif();
